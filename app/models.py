486c934d976928dc34f46451205a646636453c2a
vadimah82-dotcom <vadimah82@gmail.com>
2026-07-23 10:43:02 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,12 +1,9 @@
 from sqlalchemy import Integer, String, Text
 from sqlalchemy.orm import Mapped, mapped_column
-
 from app.database import Base
-
 
 class Recipe(Base):
     __tablename__ = "recipes"
-
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     title: Mapped[str] = mapped_column(String(255), nullable=False)
     cook_time: Mapped[int] = mapped_column(Integer, nullable=False)
