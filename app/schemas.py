d2f956ed7d5924870bc1247170a6ddc233119e18
vadimah82-dotcom <vadimah82@gmail.com>
2026-07-23 10:43:25 +0300

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -3,22 +3,18 @@ from pydantic import BaseModel, ConfigDict, Field
 
 class RecipeListItem(BaseModel):
     model_config = ConfigDict(from_attributes=True)
-
     id: int
     title: str = Field(..., description="Название блюда")
     views: int = Field(..., description="Количество просмотров детального рецепта")
     cook_time: int = Field(..., description="Время приготовления в минутах")
 
-
 class RecipeDetail(BaseModel):
     model_config = ConfigDict(from_attributes=True)
-
     id: int
     title: str = Field(..., description="Название блюда")
     cook_time: int = Field(..., description="Время приготовления в минутах")
     ingredients: list[str] = Field(..., description="Список ингредиентов")
     description: str = Field(..., description="Текстовое описание приготовления")
-
 
 class RecipeCreate(BaseModel):
     title: str = Field(..., min_length=1, description="Название блюда")
